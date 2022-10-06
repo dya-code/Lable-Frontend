b6b26d7633c2bf70bb659384dad537837611c68a
dya-code <entrydia7691@gmail.com>
2022-10-06 21:20:43 +0900

    new design

---
+++ src/components/Card.vue
@@ -0,0 +1,21 @@
+<script>
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'Card',
+    props: {
+      title: String,
+      img: String
+    }
+  })
+</script>
+  
+<template>
+  <div class="card p-10 m-10 h-[460px] bg-white rounded-3xl shadow-xl">
+    <img class="card-img rounded-xl mb-6 w-81" :src="img">
+    <div class="card-title font-bold text-2xl w-52 truncate">{{ title }}</div>
+  </div>
+</template>
+  
+<style scoped>
+</style>  
---
+++ src/views/Main.vue
@@ -0,0 +1,38 @@
+<script>
+  import { defineComponent } from 'vue'
+  import axios from 'axios'
+
+  import Card from '../components/Card.vue'
+
+  export default defineComponent({
+    components: {
+      Card
+    },
+    data: () => {
+      return {
+        res: []
+      }
+    },
+    methods: {
+    },
+    async mounted() {
+      await axios.get('http://localhost:3000/api/timetable/').then( resp => this.res = resp.data )
+
+      console.log(this.res[0].img)
+    }
+  })
+</script>
+
+<template>
+
+  <div>
+    <div class="title m-20 flex justify-center font-bold text-white text-5xl">Daily</div>
+  
+    <div class="flex justify-center items-center flex-wrap ml-[200px] mr-[200px]">
+      <Card v-for="item in res" v-bind:title="item.title" v-bind:img="item.img" />
+    </div>
+  </div>
+</template>
+  
+<style scoped>
+</style>
